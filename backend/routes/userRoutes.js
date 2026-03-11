991ea0214d006def3156d70d36a8ced5dff4618f
shafeeq <shafeeqap1986@gmail.com>
2026-03-11 06:05:25 +0530

    fix: create user issue

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,10 +11,10 @@ import {
 
 const router = express.Router();
 
-router.post("/", protect, authorize("super_admin", createUser));
-router.get("/", protect, authorize("super_admin", getUsers));
-router.get("/:id", protect, authorize("super_admin", getUserById));
-router.put("/:id", protect, authorize("super_admin", updateUserById));
-router.delete("/:id", protect, authorize("super_admin", deleteUser));
+router.post("/", protect, authorize("super_admin"), createUser);
+router.get("/", protect, authorize("super_admin"), getUsers);
+router.get("/:id", protect, authorize("super_admin"), getUserById);
+router.put("/:id", protect, authorize("super_admin"), updateUserById);
+router.delete("/:id", protect, authorize("super_admin"), deleteUser);
 
 export const userRoutes = router;
