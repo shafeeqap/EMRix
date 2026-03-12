8374285a7e3948ddbf6be772c6107114ac4653f9
shafeeq <shafeeqap1986@gmail.com>
2026-03-12 13:56:43 +0530

    feat: generateAvailableSlot logic update

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,5 +1,6 @@
 import Appointment from "../models/Appointment.js";
 import { Doctor } from "../models/Doctor.js";
+import { generateAvailableSlots } from "../service/slotService.js";
 
 // Create a new appointment
 export const createAppointment = async (req, res) => {
@@ -34,12 +35,13 @@ export const getAppointments = async (req, res) => {
 // Get available time slots for a doctor
 export const getAvailableSlots = async(req, res) =>{
   try {
-
     const { doctorId, date } = req.query;
-    // console.log(doctorId, date);
+    // console.log(doctorId, date, "getAvailableSlots query...");
     
     const slots = await generateAvailableSlots(doctorId, date);
+    // console.log(slots, "slots");
     
+    res.status(200).json({ slots });
     
   } catch (error) {
     return res.status(500).json({ message: "Server Error" });
--- backend/service/slotService.js
+++ backend/service/slotService.js
@@ -1,41 +1,50 @@
-import { Doctor } from "../models/Doctor";
+import { Doctor } from "../models/Doctor.js";
 
 export const generateAvailableSlots = async (doctorId, date) => {
+  // console.log(doctorId, date, "generateAvailableSlots");
+
   try {
-    const doctor = await Doctor.findById(doctorId);
-    // console.log(doctor);
+    const doctor = await Doctor.findOne({ userId: doctorId });
+    console.log(doctor, "doctor in generateAvailableSlots");
+
     if (!doctor) {
-      throw new Error("Doctor not found");
+      return res.status(404).json({ message: "Doctor not found" });
     }
 
     const workingHours = doctor.workingHours;
-    const startTime = workingHours.start; // e.g., "09:00"
-    const endTime = workingHours.end; // e.g., "13:00"
+    const start = workingHours.start; // e.g., "09:00"
+    const end = workingHours.end; // e.g., "13:00"
     const slotDuration = doctor.slotDuration; // Slot duration in minutes. 15 minutes
 
-    function getTimeSlots(startTime, endTime, slotDuration) {
-      const slots = [];
+    const slots = generateSlots(start, end, slotDuration);
+    console.log(slots, "generated slots");
 
-      let [hours, minutes] = startTime.split(":").map(Number);
-      let [endHours, endMinutes] = endTime.split(":").map(Number);
-
-      let currentTime = new Date();
-      currentTime.setHours(hours, minutes, 0);
-
-      const endTime = new Date();
-      endTime.setHours(endHours, endMinutes, 0);
-
-      while (currentTime < endTime) {
-        const time = currentTime.toTimeString().slice(0, 5);
-        slots.push(time);
-
-        currentTime.setMinutes(currentTime.getMinutes() + slotDuration);
-      }
-      return slots;
-    }
-
-    const availableSlots = getTimeSlots(startTime, endTime, slotDuration);
+    return slots;
   } catch (error) {
     console.error("Error generating available slots:", error);
   }
 };
+
+// Generate time slots
+function generateSlots(start, end, slotDuration) {
+  const slots = [];
+
+  let [hours, minutes] = start.split(":").map(Number);
+  let [endHours, endMinutes] = end.split(":").map(Number);
+
+  let currentTime = new Date();
+  currentTime.setHours(hours, minutes, 0);
+
+  const endTime = new Date();
+  endTime.setHours(endHours, endMinutes, 0);
+
+  while (currentTime < endTime) {
+    const time = currentTime.toTimeString().slice(0, 5);
+
+    slots.push(time);
+
+    currentTime.setMinutes(currentTime.getMinutes() + slotDuration);
+  }
+  
+  return slots;
+}
