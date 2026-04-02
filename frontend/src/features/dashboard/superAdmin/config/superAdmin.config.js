996597b02412115d189c79b383f77fba144f1d73
shafeeq <shafeeqap1986@gmail.com>
2026-04-02 07:06:58 +0530

    update: super admin and doctor dahsboard

--- frontend/src/features/dashboard/superAdmin/config/superAdmin.config.js
+++ frontend/src/features/dashboard/superAdmin/config/superAdmin.config.js
@@ -58,3 +58,78 @@ export const superAdminDashboard = [
     dataKey: "patients",
   },
 ];
+
+export const superAdminData = [
+  {
+    name: "Jan",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Feb",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Mar",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Apr",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "May",
+    uv: 1890,
+    pv: 5900,
+    amt: 2181,
+  },
+  {
+    name: "Jun",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  // {
+  //   name: "Jul",
+  //   uv: 6490,
+  //   pv: 5300,
+  //   amt: 2100,
+  // },
+  // {
+  //   name: "Aug",
+  //   uv: 3370,
+  //   pv: 4390,
+  //   amt: 2400,
+  // },
+  // {
+  //   name: "Sep",
+  //   uv: 3490,
+  //   pv: 8300,
+  //   amt: 2100,
+  // },
+  // {
+  //   name: "Oct",
+  //   uv: 5490,
+  //   pv: 7300,
+  //   amt: 2100,
+  // },
+  // {
+  //   name: "Nov",
+  //   uv: 7490,
+  //   pv: 4300,
+  //   amt: 2100,
+  // },
+  // {
+  //   name: "Dec",
+  //   uv: 3200,
+  //   pv: 4370,
+  //   amt: 2190,
+  // },
+];
